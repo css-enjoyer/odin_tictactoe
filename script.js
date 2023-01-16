b905f6bd47639886077ea734fc9a7f37a11c1938
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-01-16 11:55:44 +0800

    Add basic mark on click

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const boxes = document.querySelectorAll(".box");
 
 boxes.forEach(button => {
     button.addEventListener('click', function() {
-      console.log("Test");
+      button.innerHTML = "✖";
     }); 
 });
 

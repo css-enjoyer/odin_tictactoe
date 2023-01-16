1f2ebddcc935e269915e3eef0f9a2b6f6ae1812b
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-01-16 12:36:29 +0800

    Create JavaScript object for gameboard

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const boxes = document.querySelectorAll(".box");
 
+// ------ Game Object ------
+const game = {
 
-boxes.forEach(button => {
-    button.addEventListener('click', function() {
-      button.innerHTML = "✖";
-    }); 
+};
+
+// ------ Players Object ------
+const players = {
+    player1: "O",
+    player2: "✖",
+};
+
+// ------ Board Object ------
+const board = {
+    cells: []
+};
+for (let i = 0; i < 9; i++) {
+    const cell = document.querySelector(`#cell-${i}`);
+    board.cells.push(cell);
+}
+board.cells.forEach(function(cell) {
+    cell.addEventListener('click', function() {
+        // console.log(cell);
+        cell.innerHTML = "✖";
+    });
 });
-
-console.log("End");

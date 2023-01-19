8c80bed2fd4df224cd60b34051a0b3e1cd84ae6b
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-01-19 12:58:29 +0800

    Complete brute force win conditions

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ let boardObject = {
     cell6: 0, cell7: 0, cell8: 0,  
 };
 // ## Select each cell and insert into array
+// ##### TO DO: Refine and compress this block, make checking win conditions into its own function
 let cells = [];
 for (let i = 0; i < 9; i++) {
     cells[i] = document.querySelector(`#cell-${i}`);
@@ -48,10 +49,9 @@ for (let i = 0; i < 9; i++) {
                 boardObject["cell" + i] = 2;
             }
         }
-        if(boardObject.cell0 === 1 && boardObject.cell1 === 1 && boardObject.cell2 === 1) {
-            console.log("X Wins");
-            console.log(boardObject.cell0);
-        }
+        if(checkWin()) {
+            gameEnd();
+        };
     });
 };
 
@@ -67,8 +67,85 @@ let gameBoard = {
     gameStart: false,
     gameEnd: false
 };
-
+// ## GameCycle 
+function gameEnd() {
+    console.log(test);
+};
 // ## Win Conditions
+function checkWin() {
+    // ### Horizontal Alignments
+    if (boardObject.cell0 !== 0 && boardObject.cell0 === boardObject.cell1 && boardObject.cell1 === boardObject.cell2) {
+        if(boardObject.cell0 === 1) {
+            console.log("X wins - horizontal line");
+
+        }
+        else if(boardObject.cell0 === 2) {
+            console.log("O wins - horizontal line");
+        }
+    } else if (boardObject.cell3 !== 0 && boardObject.cell3 === boardObject.cell4 && boardObject.cell4 === boardObject.cell5) {
+        if(boardObject.cell3 === 1) {
+            console.log("X wins - horizontal line");
+        }
+        else if(boardObject.cell3 === 2) {
+            console.log("O wins - horizontal line");
+        }
+    } else if (boardObject.cell6 !== 0 && boardObject.cell6 === boardObject.cell7 && boardObject.cell7 === boardObject.cell8) {
+        if(boardObject.cell6 === 1) {
+            console.log("X wins - horizontal line");
+        }
+        else if(boardObject.cell6 === 2) {
+            console.log("O wins - horizontal line");
+        }
+    }  
+    // ### Vertical Alignments
+    if (boardObject.cell0 !== 0 && boardObject.cell0 === boardObject.cell3 && boardObject.cell3 === boardObject.cell6) {
+        if(boardObject.cell0 === 1) {
+            console.log("X wins - vertical line");
+        }
+        else if(boardObject.cell0 === 2) {
+            console.log("O wins - vertical line");
+        }
+    } else if (boardObject.cell1 !== 0 && boardObject.cell1 === boardObject.cell4 && boardObject.cell4 === boardObject.cell7) {
+        if(boardObject.cell1 === 1) {
+            console.log("X wins - vertical line");
+        }
+        else if(boardObject.cell1 === 2) {
+            console.log("O wins - vertical line");
+        }
+    } else if (boardObject.cell2 !== 0 && boardObject.cell2 === boardObject.cell5 && boardObject.cell5 === boardObject.cell8) {
+        if(boardObject.cell2 === 1) {
+            console.log("X wins - vertical line");
+        }
+        else if(boardObject.cell2 === 2) {
+            console.log("O wins - vertical line");
+        }
+    }  
+    // ### Diagonal Alignments
+    if (boardObject.cell0 !== 0 && boardObject.cell0 === boardObject.cell4 && boardObject.cell4 === boardObject.cell8) {
+        if(boardObject.cell0 === 1) {
+            console.log("X wins - diagonal line");
+        }
+        else if(boardObject.cell0 === 2) {
+            console.log("O wins - diagonal line");
+        }
+    } else if (boardObject.cell2 !== 0 && boardObject.cell2 === boardObject.cell4 && boardObject.cell4 === boardObject.cell6) {
+        if(boardObject.cell2 === 1) {
+            console.log("X wins - diagonal line");
+        }
+        else if(boardObject.cell2 === 2) {
+            console.log("O wins - diagonal line");
+        }
+    } else if (boardObject.cell2 !== 0 && boardObject.cell2 === boardObject.cell5 && boardObject.cell5 === boardObject.cell8) {
+        if(boardObject.cell2 === 1) {
+            console.log("X wins - diagonal line");
+        }
+        else if(boardObject.cell2 === 2) {
+            console.log("O wins - diagonal line");
+        }
+    }  
+};
+
+
 
 
 

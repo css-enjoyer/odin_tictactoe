71cb8a2aaef8de867ae163ac7e7d7569f551edc7
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-01-19 15:46:27 +0800

    Highlight winning cells

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ for (let i = 0; i < 9; i++) {
                 boardObject["cell" + i] = 2;
             }
         }
-        if(checkWin()) {
-            gameEnd();
+        if(winDetails = checkWin()) {
+            gameEnd(winDetails);
         } else if (checkTie()) {
             gameTie();
         }
@@ -54,13 +54,21 @@ popup.addEventListener("click", () => {
     }
     for (let i = 0; i < 9; i++) {
         document.querySelector(`#cell-${i}`).innerHTML = "";
-        console.log(`#cell-${i}`);
+        // console.log(`#cell-${i}`);
     }
+    winDetails.array.forEach(element => {
+        element.classList.toggle("winning-cells");
+    });
 });
-function gameEnd() {
+function gameEnd(winDetails) {
+    document.querySelector(".popup h1").innerHTML = winDetails.string + " wins!";
     popup.classList.toggle("popup-visible");
+    winDetails.array.forEach(element => {
+        element.classList.toggle("winning-cells");
+    });
 };
 function gameTie() {
+    document.querySelector(".popup h1").innerHTML = "Tie game!";
     popup.classList.toggle("popup-visible");
 }
 
@@ -70,87 +78,78 @@ function checkWin() {
     if (boardObject.cell0 !== 0 && boardObject.cell0 === boardObject.cell1 && boardObject.cell1 === boardObject.cell2) {
         if(boardObject.cell0 === 1) {
             console.log("X wins - horizontal line");
-            return true;
+            return {string: "X", array: [cells[0], cells[1], cells[2]]};
         }
         else if(boardObject.cell0 === 2) {
             console.log("O wins - horizontal line");
-            return true;
+            return {string: "O", array: [cells[0], cells[1], cells[2]]};
         }
     } else if (boardObject.cell3 !== 0 && boardObject.cell3 === boardObject.cell4 && boardObject.cell4 === boardObject.cell5) {
         if(boardObject.cell3 === 1) {
             console.log("X wins - horizontal line");
-            return true;
+            return {string: "X", array: [cells[3], cells[4], cells[5]]};
         }
         else if(boardObject.cell3 === 2) {
             console.log("O wins - horizontal line");
-            return true;
+            return {string: "O", array: [cells[3], cells[4], cells[5]]};
         }
     } else if (boardObject.cell6 !== 0 && boardObject.cell6 === boardObject.cell7 && boardObject.cell7 === boardObject.cell8) {
         if(boardObject.cell6 === 1) {
             console.log("X wins - horizontal line");
-            return true;
+            return {string: "X", array: [cells[6], cells[7], cells[8]]};
         }
         else if(boardObject.cell6 === 2) {
             console.log("O wins - horizontal line");
-            return true;
+            return {string: "O", array: [cells[6], cells[7], cells[8]]};
         }
     }  
     // ### Vertical Alignments
     if (boardObject.cell0 !== 0 && boardObject.cell0 === boardObject.cell3 && boardObject.cell3 === boardObject.cell6) {
         if(boardObject.cell0 === 1) {
             console.log("X wins - vertical line");
-            return true;       
+            return {string: "X", array: [cells[0], cells[3], cells[6]]};
         }
         else if(boardObject.cell0 === 2) {
             console.log("O wins - vertical line");
-            return true;       
+            return {string: "O", array: [cells[0], cells[3], cells[6]]};
         }
     } else if (boardObject.cell1 !== 0 && boardObject.cell1 === boardObject.cell4 && boardObject.cell4 === boardObject.cell7) {
         if(boardObject.cell1 === 1) {
             console.log("X wins - vertical line");
-            return true;       
+            return {string: "X", array: [cells[1], cells[4], cells[7]]};
         }
         else if(boardObject.cell1 === 2) {
             console.log("O wins - vertical line");
-            return true;       
+            return {string: "O", array: [cells[1], cells[4], cells[7]]};
         }
     } else if (boardObject.cell2 !== 0 && boardObject.cell2 === boardObject.cell5 && boardObject.cell5 === boardObject.cell8) {
         if(boardObject.cell2 === 1) {
             console.log("X wins - vertical line");
-            return true;       
+            return {string: "X", array: [cells[2], cells[5], cells[8]]};
         }
         else if(boardObject.cell2 === 2) {
             console.log("O wins - vertical line");
-            return true;       
+            return {string: "O", array: [cells[2], cells[5], cells[8]]};
         }
     }  
     // ### Diagonal Alignments
     if (boardObject.cell0 !== 0 && boardObject.cell0 === boardObject.cell4 && boardObject.cell4 === boardObject.cell8) {
         if(boardObject.cell0 === 1) {
             console.log("X wins - diagonal line");
-            return true;       
+            return {string: "X", array: [cells[0], cells[4], cells[8]]};
         }
         else if(boardObject.cell0 === 2) {
             console.log("O wins - diagonal line");
-            return true;       
+            return {string: "O", array: [cells[0], cells[4], cells[8]]};
         }
     } else if (boardObject.cell2 !== 0 && boardObject.cell2 === boardObject.cell4 && boardObject.cell4 === boardObject.cell6) {
         if(boardObject.cell2 === 1) {
             console.log("X wins - diagonal line");
-            return true;       
+            return {string: "X", array: [cells[2], cells[4], cells[6]]};
         }
         else if(boardObject.cell2 === 2) {
             console.log("O wins - diagonal line");
-            return true;       
-        }
-    } else if (boardObject.cell2 !== 0 && boardObject.cell2 === boardObject.cell5 && boardObject.cell5 === boardObject.cell8) {
-        if(boardObject.cell2 === 1) {
-            console.log("X wins - diagonal line");
-            return true;       
-        }
-        else if(boardObject.cell2 === 2) {
-            console.log("O wins - diagonal line");
-            return true;       
+            return {string: "O", array: [cells[2], cells[4], cells[6]]};
         }
     }  
 };
